53c836697e1c2038a8a221cc5b3c3c66ec73d3cd
Zhao Li <zhao.li@startingspark.com>
2020-06-26 05:02:56 -1000

    got basic students and grades get requests working

--- index.js
+++ index.js
@@ -2,6 +2,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let students = [
+  {
+    id: 1,
+    name: "Brandon"
+  },
+  {
+    id: 2,
+    name: "Jeff"
+  }
+];
+
+let grades = [
+  {
+    id: 1,
+    math: "A+",
+    science: "A-",
+    "language-arts": "A"
+  },
+  {
+    id: 2,
+    math: "A-",
+    science: "A",
+    "language-arts": "A-"
+  }
+];
+
+
+app.get('/students/', (req, res) => {
+  res.send(students)
+});
+
+app.get('/students/:studentId', (req, res) => {
+  const studentId = req.params.studentId;
+  const actualIndex = studentId - 1; // account for 0-indexing
+  res.send(students[actualIndex])
+});
+
+app.get('/grades/:studentId', (req, res) => {
+  const studentId = req.params.studentId;
+  const actualIndex = studentId - 1; // account for 0-indexing
+  res.send(grades[actualIndex])
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
